b60d9e516e133945436be7bac8c93c9d51954902
Luis.Ramirez <l.ramirez@schenker-tech.de>
2023-02-09 21:41:09 +0100

    conditional rendering list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,22 +2,30 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("0");
+
   const addSelectedYear = (year) => {
-    console.log(year);
+    setSelectedYear(year);
   };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    if (selectedYear == "0") {
+      return props.expenses;
+    } else {
+      return expense.date.getFullYear().toString() === selectedYear;
+    }
+  });
+
+ 
 
   return (
     <Card className="expenses">
       <ExpensesFilter onSelectedYear={addSelectedYear} />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList expenses={filteredExpenses}/>
     </Card>
   );
 };
